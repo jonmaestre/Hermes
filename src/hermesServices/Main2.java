d4f7f6abcdbf1ac6ed3e002b1ef725fc08963ba8
aitortxuh <a.hernandez@opendeusto.es>
2021-11-11 10:31:07 +0100

    editar

--- src/hermesServices/Main2.java
+++ src/hermesServices/Main2.java
@@ -5,7 +5,7 @@ public class Main2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Main2");
-
+		System.out.println("PUEDO EDITAR¿¿¿¿¿");
 	}
 
 }
