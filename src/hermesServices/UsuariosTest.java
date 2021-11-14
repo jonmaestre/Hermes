2662253818bdba50f3e6072a6ba82b83e2c23f5c
aitortxuh <a.hernandez@opendeusto.es>
2021-11-14 17:36:26 +0100

    Pruebas Junit
    no toqueis

---
+++ src/hermesServices/UsuariosTest.java
@@ -0,0 +1,14 @@
+package hermesServices;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuariosTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
