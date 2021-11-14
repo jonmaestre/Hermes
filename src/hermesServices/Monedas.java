93355e647ad4e78383b6742ece096c907698a43c
aitortxuh <a.hernandez@opendeusto.es>
2021-11-14 16:42:59 +0100

    monedas y satisfaccion

--- src/hermesServices/Monedas.java
+++ src/hermesServices/Monedas.java
@@ -1,5 +1,42 @@
 package hermesServices;
 
 public class Monedas extends Estadisticas {
+	
+	public class Monedas extends Estadisticas_totales {
+		private float cantidadObtenida = 0; // LA catidad QUe tienes
+		private float catidadRequerida = 0; // para realizar compras 
+		private String nombre_moneda = "Kromer" ;
+		
 
+		public Monedas(float satisfaccion_total, float monedas_tota, float cantidadObtenida, float catidadRequerida,
+				String nombre_moneda) {
+			super(satisfaccion_total, monedas_tota);
+			this.cantidadObtenida = cantidadObtenida;
+			this.catidadRequerida = catidadRequerida;
+			this.nombre_moneda = nombre_moneda;
+		}
+
+		public float getCantidadObtenida() {
+			return cantidadObtenida;
+		}
+
+		public void setCantidadObtenida(float cantidadObtenida) {
+			this.cantidadObtenida = cantidadObtenida;
+		}
+
+		public float getCatidadRequerida() {
+			return catidadRequerida;
+		}
+
+		public void setCatidadRequerida(float catidadRequerida) {
+			this.catidadRequerida = catidadRequerida;
+		}
+
+		public String getNombre_moneda() {
+			return nombre_moneda;
+		}
+
+		public void setNombre_moneda(String nombre_moneda) {
+			this.nombre_moneda = nombre_moneda;
+		}
 }
