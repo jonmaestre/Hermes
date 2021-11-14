5859b96b132a0dcacbd5d9607db9e4b1077a7a20
aitortxuh <a.hernandez@opendeusto.es>
2021-11-14 10:50:49 +0100

    Atributos 2
    Faltan cosas

--- src/hermesServices/Estadisticas_totales.java
+++ src/hermesServices/Estadisticas_totales.java
@@ -1,5 +1,31 @@
 package hermesServices;
 
 public class Estadisticas_totales extends Estadisticas {
+	private float satisfaccion_total = 0;
+	private float monedas_tota = 0;
+	
+	public Estadisticas_totales(float satisfaccion_total, float monedas_tota) {
+		super();
+		this.satisfaccion_total = satisfaccion_total;
+		this.monedas_tota = monedas_tota;
+	}
 
+	public float getSatisfaccion_total() {
+		return satisfaccion_total;
+	}
+
+	public void setSatisfaccion_total(float satisfaccion_total) {
+		this.satisfaccion_total = satisfaccion_total;
+	}
+
+	public float getMonedas_tota() {
+		return monedas_tota;
+	}
+
+	public void setMonedas_tota(float monedas_tota) {
+		this.monedas_tota = monedas_tota;
+	}
+	
+	
+	
 }
--- src/hermesServices/Satisfaccion.java
+++ src/hermesServices/Satisfaccion.java
@@ -1,5 +1,32 @@
 package hermesServices;
 
-public class Satisfaccion extends Estadisticas {
+public class Satisfaccion extends Estadisticas_totales {
+	private String nombre_satisfaccion = ""; // estilo muy satisfactorio,satisfactorio,normal, mal, muy mal igual hay que ponerlo en un enum
+	private float catidadSatisfactoria = 100 ; // he pensado en empezar con 100 % y que cuando no cumpla un requisito baje
+	
+	public Satisfaccion(float satisfaccion_total, float monedas_tota, String nombre_satisfaccion,
+			float catidadSatisfactoria) {
+		super(satisfaccion_total, monedas_tota);
+		this.nombre_satisfaccion = nombre_satisfaccion;
+		this.catidadSatisfactoria = catidadSatisfactoria;
+	}
+
+	public String getNombre_satisfaccion() {
+		return nombre_satisfaccion;
+	}
+
+	public void setNombre_satisfaccion(String nombre_satisfaccion) {
+		this.nombre_satisfaccion = nombre_satisfaccion;
+	}
+
+	public float getCatidadSatisfactoria() {
+		return catidadSatisfactoria;
+	}
+
+	public void setCatidadSatisfactoria(float catidadSatisfactoria) {
+		this.catidadSatisfactoria = catidadSatisfactoria;
+	}
+	
+	
 
 }
