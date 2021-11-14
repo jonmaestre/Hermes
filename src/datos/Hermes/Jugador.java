812fd0f91a08d509e6aaf0bb14148fd39430e883
komsomol6 <nicolas.azlor@opendeusto.es>
2021-11-14 18:52:03 +0100

    SAVE/DELETE

--- src/datos/Hermes/Jugador.java
+++ src/datos/Hermes/Jugador.java
@@ -2,21 +2,25 @@ package datos.Hermes;
 
 public class Jugador extends Datos {
 	
-	private int id = 0;
-	private String nombre = "";
-	private int nivel = 0;
-	private int cartera = 0; // Monedas que posee el jugador
+	protected int idJugador;
+	protected String nombre;
+	protected int nivel;
+	protected int exp = 0;
+	protected int cartera = 0; 
+	// Monedas que posee el jugador
 	// Hacer lista de entidades objeto
 	private int dia = 0;
 	
-	public int getId() {
-		return id;
-	}
 	
-	public void setId(int id) {
-		this.id = id;
-	}
 	
+	public int getIdJugador() {
+		return idJugador;
+	}
+
+	public void setIdJugador(int idJugador) {
+		this.idJugador = idJugador;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -30,9 +34,50 @@ public class Jugador extends Datos {
 	}
 	
 	public void setNivel(int nivel) {
-		this.nivel = nivel;
+		if (this.exp <= 10) {
+			this.nivel = 0;
+			
+		}
+		else if (this.exp <= 100) {
+			this.nivel = 1;
+		}
+		else if (this.exp <= 200) {
+			this.nivel = 2;
+		}
+		else if (this.exp <= 300) {
+			this.nivel = 3;
+		}
+		else if (this.exp <= 400) {
+			this.nivel = 4;
+		}
+		else if (this.exp <= 500) {
+			this.nivel = 5;
+		}
+		else if (this.exp <= 600) {
+			this.nivel = 6;
+		}
+		else if (this.exp <= 700) {
+			this.nivel = 7;
+		}
+		else if (this.exp <= 800) {
+			this.nivel = 7;
+		}
+		else if (this.exp <= 900) {
+			this.nivel = 9;
+		}
+		else {
+			this.nivel = 10;
+		}
 	}
 	
+	public int getExp() {
+		return exp;
+	}
+
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
+
 	public int getCartera() {
 		return cartera;
 	}
@@ -48,5 +93,22 @@ public class Jugador extends Datos {
 	public void setDia(int dia) {
 		this.dia = dia;
 	}
+
+	public Jugador(int idJugador, String nombre, int nivel, int exp, int cartera, int dia) {
+		super();
+		this.idJugador = idJugador;
+		this.nombre = nombre;
+		this.nivel = nivel;
+		this.exp = exp;
+		this.cartera = cartera;
+		this.dia = dia;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [ID="+idJugador +"nombre=" + nombre + ", nivel=" + nivel + ", exp=" + exp
+				+ ", cartera=" + cartera + ", dia=" + dia + "]";
+	}
+	
 	
 }
--- src/hermesServices/Juego.java
+++ src/hermesServices/Juego.java
@@ -1,11 +1,91 @@
 package hermesServices;
 
-public class Juego extends Main implements Runnable{
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+import datos.Hermes.Jugador;
+
+public class Juego{
+	
+	protected ArrayList<Jugador> jugadores;
+	private HashMap<Integer,String> clasJugador;
+	
+	public Juego(ArrayList<Jugador> jugadores,HashMap<Integer,String> clasJugador) {
+		super();
+		this.jugadores = jugadores;
+		this.clasJugador = clasJugador;
 	}
+	public Juego() {
+		super();
+		this.jugadores = new ArrayList<Jugador>();
+		this.clasJugador = new HashMap<Integer,String>();
+	}
+	public ArrayList<Jugador> getJugadores() {
+		return jugadores;
+	}
+	public void setJugadores(ArrayList<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+	
+	public void cargarDatosJugadores() {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			fr = new FileReader("saveslot.csv");
+			br = new BufferedReader(fr);
+			
+			String linea = br.readLine();
+			
+			while (linea != null) {
+				String[] campos = linea.split(",");
+				
+				Jugador nuevo = new Jugador(0, linea, 0, 0, 0, 0);
+				nuevo.setIdJugador(Integer.parseInt(campos[0]));
+				nuevo.setNombre(campos[1]);
+				nuevo.setNivel(Integer.parseInt(campos[2]));
+				nuevo.setExp(Integer.parseInt(campos[3]));
+				nuevo.setCartera(Integer.parseInt(campos[4]));
+				nuevo.setDia(Integer.parseInt(campos[5]));
+				
+				anyadirJugador(nuevo);
+				
+				linea = br.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void anyadirJugador(Jugador j) {
+		jugadores.add(j);
+		clasJugador.put(j.getIdJugador(), j.getNombre());
+	}
+	
+	
+	public void eliminarJugador(Jugador j) {
+		jugadores.remove(j);
+		clasJugador.remove(j.getIdJugador());
+
+	}
+	
+	public void guardarDatosJugadores() {
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter("saveslot.csv");
+			
+			for (Jugador j : jugadores) {
+				pw.println(j);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pw.close();
+		}
+	}
+	
+
 	
 }
