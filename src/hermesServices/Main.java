64e0472e326c0fa4620436e9de41f0326e1b6a02
jonmaestre <jonmaestre@opendeusto.es>
2021-11-18 10:46:16 +0100

    Update

--- src/hermesServices/Main.java
+++ src/hermesServices/Main.java
@@ -5,9 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("HERMES - SERVICES");
-		System.out.println("Main 1");
-		System.out.println("HOLAMUNDO");
 		ventanaInicial v= new ventanaInicial(100,200,"Hermes");
 		VentanaJuego1 Ventana = new VentanaJuego1 (200, 200);
 	}
